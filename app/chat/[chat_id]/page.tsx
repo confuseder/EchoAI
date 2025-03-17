36e46364eb1b8a34e38447f2732c3f2b4f7be044
Acbox <me@acbox.lol>
2025-03-18 03:59:11 +0800

    fix: blank module

---
+++ app/chat/[chat_id]/page.tsx
@@ -0,0 +1,11 @@
+export default function ChatPage() {
+  return (
+    <div className="w-full h-full">
+      <div className="w-full h-full flex flex-col">
+        <div className="w-full h-16 bg-white rounded-lg">
+          <h1>Chat</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
