d53bbf735d26218e0e6b8f052a431d70fe7e3923
Acbox <acbox0328@gmail.com>
2025-04-25 15:19:42 +0800

    feat: add function component

--- database/api/common/canvas/function/__init__.py
+++ database/api/common/canvas/function/__init__.py
@@ -1,5 +1,7 @@
 from .function import function
+from .save_as import save_as
 
 function_set = [
   function,
+  save_as,
 ]
---
+++ database/api/common/canvas/function/function.py
@@ -0,0 +1,5 @@
+from api import comp
+
+function = comp('function', 'A function component')
+function.attr('expr', 'The expression of the function', '(x: number) => number')
+function.attr('domain', 'The domain of the function', '[number, number]')
