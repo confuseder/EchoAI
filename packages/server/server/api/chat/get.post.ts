45ecf48d6d606461ab4a3a0111a3a2f7f777a634
盼兮 <63710193+confuseder@users.noreply.github.com>
2025-05-30 15:12:47 +0800

    优化 CORS 和身份验证中间件，更新依赖项，移除未使用的代码，优化导入结构。 (#6)
    * 修复了用户信息展示框关闭后仍遮挡主输入框的错误
    
    * 优化 CORS 和身份验证中间件，更新依赖项，移除未使用的代码，优化导入结构。

--- packages/server/server/api/chat/get.post.ts
+++ packages/server/server/api/chat/get.post.ts
@@ -3,7 +3,6 @@ import { table as chats } from "../../../db/chats";
 import db from "../../../db";
 import { DesignerStep, StepBranch } from "@echoai/workflow/designer";
 import { ChatCompletionMessageParam } from "openai/resources.mjs";
-import { UNAUTHORIZED_MODE, UNAUTHORIZED_MODE_USER_ID } from "@echoai/utils";
 
 export type Message = ChatCompletionMessageParam;
 export type Context = Message[];
