8adc1db4e8352089e8be6834e8840a0efd88dbe4
Acbox <acbox0328@gmail.com>
2025-06-06 15:58:22 +0800

    refactor: operations process

--- components/Board.vue
+++ components/Board.vue
@@ -10,10 +10,12 @@
 <script setup lang="ts">
 import type { Operation } from '~/types';
 import { Whiteboard } from './whiteboard';
+import { renderRoots } from 'sciux';
 
 const boardRef = ref<HTMLDivElement>()
+const executed = reactive<string[]>([])
 
-defineProps<{
+const props = defineProps<{
   operations: Operation[]
   pageId: string
   whiteboard: Whiteboard
@@ -22,4 +24,44 @@ defineProps<{
 defineEmits<{
   (e: 'switch', direction: 'previous' | 'next'): void
 }>()
+
+const render = (pageId: string) => {
+  if (!props.whiteboard) return
+  const { document } = props.whiteboard.findPage(pageId)!
+  if (!document) return
+  try {
+    const roots = renderRoots(document.children)
+    boardRef.value!.replaceChildren(...roots)
+  } catch (e) {
+    console.error('Failed to render page', pageId, 'error:', e)
+  }
+}
+
+effect(() => {
+  if (props.operations.length < 1) return
+  for (const opt of props.operations) {
+    if (executed.includes(opt.id)) continue
+    switch (opt.type) {
+      case 'add-node':
+        props.whiteboard.addNode(props.pageId, opt.position, opt.content)
+        break
+      case 'remove-node':
+        props.whiteboard.removeNode(props.pageId, opt.position)
+        break
+      case 'set-prop':
+        props.whiteboard.setProp(props.pageId, opt.position, opt.attr, opt.value)
+        break
+      case 'set-content':
+        props.whiteboard.setContent(props.pageId, opt.position, opt.content)
+        break
+      case 'remove-prop':
+        props.whiteboard.removeProp(props.pageId, opt.position, opt.attr)
+        break
+      default:
+        console.error('Unknown operation:', opt)
+    }
+    executed.push(opt.id)
+  }
+  render(props.pageId)
+})
 </script>
